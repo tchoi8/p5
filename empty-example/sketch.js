2d67f3c91d7a9cec5d524c46feafaa05a19d7845
Taeyoon Choi <shootmegame@gmail.com>
2014-08-19 20:16:55 -0400

    p5.js for everyone

---
+++ empty-example/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // put setup code here
+ createCanvas(640, 480);
+}
+
+function draw() {
+  // put drawing code here
+
+ 
+  if (mouseIsPressed) {
+    fill(0);
+//    noStroke();
+      ellipse(mouseX, mouseY, 20, 20);
+  } else {
+ 
+  }
+ 
+}
